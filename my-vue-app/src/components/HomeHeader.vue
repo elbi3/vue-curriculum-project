afaf3f7a36ca085b336eacd4331098183c7f13ba
El Brewster <167570227+elbi3@users.noreply.github.com>
2026-05-18 20:03:29 -0700

    Refactor header and home nav to move toggle upper right

--- my-vue-app/src/components/HomeHeader.vue
+++ my-vue-app/src/components/HomeHeader.vue
@@ -1,5 +1,12 @@
+<script setup>
+import ThemeToggle from './ThemeToggle.vue';
+</script>
+
 <template>
     <header class="home-header">
-        <h1 class="home-title">Vue App Playground</h1>       
+        <div class="title-div">
+            <h1 class="home-title">Vue App Playground</h1>       
+        </div>
+        <ThemeToggle />
     </header>
 </template>
--- my-vue-app/src/views/Home.vue
+++ my-vue-app/src/views/Home.vue
@@ -4,25 +4,21 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-    <nav>
-
-    </nav>
     <main class="home-main">
         <h2>hello! this is a learning space for the Vue metaframework</h2>
         <p>if you would like to see code to study any of the following topics, hit the links :D</p>
-        <ul>
-            <li><RouterLink to="/dogs">Take me to see stuff about dogs!</RouterLink></li>
-            <li><RouterLink to="/shopping">I want to see my shopping list!</RouterLink></li>
-            <li><RouterLink to="/tasks">I want to see my task manager app!</RouterLink></li>
-            <li><RouterLink to="/subscriber">I would like to change my subscriber status.</RouterLink></li>
-            <li><RouterLink to="/user-profile">I want to update my address.</RouterLink></li>
-            <li><RouterLink to="/gallery">Let's look at the recipe gallery.</RouterLink></li>
-            <li><RouterLink to="/gallery">Gallery</RouterLink></li>
-            <li><RouterLink to="/gallery">Gallery</RouterLink></li>
-            <li><RouterLink to="/gallery">Gallery</RouterLink></li>
-            <li><RouterLink to="/gallery">Gallery</RouterLink></li>
-
-        </ul>
+        <nav class="topical-site-navigation">
+            <ul class="home-content-links">
+                <li><RouterLink to="/dogs">➡️ Take me to see stuff about dogs!</RouterLink></li>
+                <li><RouterLink to="/shopping">➡️ I want to see my shopping list!</RouterLink></li>
+                <li><RouterLink to="/tasks">➡️ I want to see my task manager app!</RouterLink></li>
+                <li><RouterLink to="/subscriber">➡️ I would like to change my subscriber status.</RouterLink></li>
+                <li><RouterLink to="/user-profile">➡️ I want to update my address.</RouterLink></li>
+                <li><RouterLink to="/gallery">➡️ Let's look at the recipe gallery.</RouterLink></li>
+                <li><RouterLink to="/blog">➡️ I want to look at the blog.</RouterLink></li>
+                <li><RouterLink to="/counter">➡️ I want play with counters.</RouterLink></li>
+            </ul>
+        </nav>
         <RouterView />
     </main>
 
