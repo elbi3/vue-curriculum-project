8a33ba3a564d1cdd9a536dee4f8ff7311e8463f5
El Brewster <167570227+elbi3@users.noreply.github.com>
2026-05-30 11:21:27 -0700

    Update project docs

--- my-vue-app/src/views/Playground.vue
+++ my-vue-app/src/views/Playground.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
 import Input from "@/components/Input.vue";
+import { h } from "vue";
+
+
+const myDiv = h("div", {class:"my-div", innerHTML: "my div"});
 </script>
 
 <template>
+    {{myDiv.type}}
     <div class="playground-container">
         <Input />
     </div>
---
+++ vee_validate_form_vue.md
@@ -0,0 +1,4 @@
+
+
+1. install vee validate with `npm i vee-validate --save`
+[vee validate](https://vee-validate.logaretm.com/v4/guide/overview/)
